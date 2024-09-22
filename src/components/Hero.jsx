80d0699372891a991149e45e7d6ee26e3e81d370
asifrahman <asif.rahman@dimiour.io>
2024-09-22 22:35:54 +0530

    csaz

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/asifCut.png";
+import profilePic from "../assets/royadPhoto.jpg";
 import { motion } from 'framer-motion';
 const container = (delay) => ({
   hidden:{x:-100,opacity:0},
